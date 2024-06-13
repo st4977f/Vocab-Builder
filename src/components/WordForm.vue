f4a8b2893846007e834f9d0e8e06d986078cda5e
Sule Tunkara <9588380+st4977f@users.noreply.github.com>
2024-06-13 02:15:37 +0100

    Ready to deploy

---
+++ src/components/WordForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <form action="#" @submit.prevent="onSubmit">
+     <p v-if="errorsPresent" class="error">Please fill out all fields!</p>
+ 
+     <div class="ui labeled input fluid">
+       <div class="ui label">
+         <i class="germany flag"></i> German
+       </div>
+       <input type="text" placeholder="Enter word..." v-model="word.german" />
+     </div>
+ 
+     <div class="ui labeled input fluid">
+       <div class="ui label">
+         <i class="united kingdom flag"></i> English
+       </div>
+       <input type="text" placeholder="Enter word..." v-model="word.english" />
+     </div>
+
+     <div class="ui labeled input fluid">
+       <div class="ui label">
+         <i class="spain flag"></i> Spanish
+       </div>
+       <input type="text" placeholder="Enter word..." v-model="word.spanish" />
+     </div>
+ 
+     <button class="positive ui button">Submit</button>
+   </form>
+ </template>
+ 
+ <script>
+ export default {
+   name: 'word-form',
+   props: {
+     word: {
+       type: Object,
+       required: false,
+       default: () => {
+        return {
+          english: '',
+          german: '',
+          spanish: ''
+        };
+       }
+     }
+   },
+   data() {
+     return {
+       errorsPresent: false
+     };
+   },
+   methods: {
+     onSubmit: function() {
+      if (this.word.english === '' || this.word.german == '' || this.word.spanish == '') {
+        this.errorsPresent = true;
+      } else {
+        this.$emit('createOrUpdate', this.word)
+      }
+       //console.log(`English: ${this.word.english}`);
+       //console.log(`German: ${this.word.german}`);
+     }
+   }
+ };
+ </script>
+ 
+ <style scoped>
+ .error {
+   color: red;
+ }
+ </style>
---
+++ src/views/User.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>This Page is an example of a Vue.js application that uses Vue Router and Vuex. It is a vocabulary learning app that allows users to add words to a list and then test themselves on those words.</p>
+        <p>It was created by Suleman Tunkara<a href="https://github.com/st4977f"> - Github</a></p>
+    </div>
+</template>
+
