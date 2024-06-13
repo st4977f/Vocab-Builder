861cc335f6dde0da47b38ecf241593a3ee0572a3
Sule Tunkara <9588380+st4977f@users.noreply.github.com>
2024-06-13 02:20:06 +0100

    Git changes

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>About</h1>
         <p>This Page is an example of a Vue.js application that uses Vue Router and Vuex. It is a vocabulary learning app that allows users to add words to a list and then test themselves on those words.</p>
-        <p>It was created by Suleman Tunkara<a href="https://github.com/st4977f"> - Github</a></p>
+        <p>It was created by Suleman Tunkara<a href="https://github.com/st4977f/Vocab-Builder"> - Github</a></p>
     </div>
 </template>
 
