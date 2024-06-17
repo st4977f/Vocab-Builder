3fb53855189737116a63d64233f56b0c51f0dac2
Sule Tunkara <9588380+st4977f@users.noreply.github.com>
2024-06-17 03:26:55 +0100

    About edit

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,10 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
     <div>
         <h1>About</h1>
-        <p>This Page is an example of a Vue.js application that uses Vue Router and Vuex. It is a vocabulary learning app that allows users to add words to a list and then test themselves on those words.</p>
+        <p>This page is a MEVN Stack application that uses Vue Router, Vuex and MongoDB. </p>
+        <p>It is a vocabulary learning app that allows users to add words to a list and then test themselves on those words.</p>
+        <p>Full CRUD is available only to show capabilities, there is a database reset mechanism in place.</p>
         <p>It was created by Suleman Tunkara<a href="https://github.com/st4977f/Vocab-Builder"> - Github</a></p>
     </div>
 </template>
