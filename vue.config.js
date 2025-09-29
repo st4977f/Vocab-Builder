6f4c756398064f978bf32c8ccf31a221643880d9
Sule Tunkara <st.tunkara@gmail.com>
2025-09-29 18:28:53 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_BASE_URL || '/',
   lintOnSave: false,
   transpileDependencies: true,
+  
+  devServer: {
+    port: 3003
+  }
 });
