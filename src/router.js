7ab6d65ae608823ee8d4c9f54af066dbff5df396
Sule Tunkara <st.tunkara@gmail.com>
2025-09-30 14:54:28 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Words from "/views/Words.vue";
+import Words from "./views/Words.vue";
 import New from "./views/New.vue";
 import Show from "./views/Show.vue";
 import Edit from "./views/Edit.vue";
