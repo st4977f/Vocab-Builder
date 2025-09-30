fa0a2ef279b99594ccfc74c3faf90931d19fa137
Sule Tunkara <st.tunkara@gmail.com>
2025-09-30 13:31:29 +0100

    Update helpers.js

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -11,7 +11,7 @@ Vue.use(VueFlashMessage, {
 });
 
 const vm = new Vue();
-const baseURL = process.env.VUE_APP_BASE_URL;
+const baseURL = process.env.VUE_APP_API_BASE_URL || '/vocab/api/';;
 
 const handleError =
   (fn) =>
